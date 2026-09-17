820d4594b82381a76d4f56ac3342c056368eeec1
Crizzz3 <tamayoavalosjose621@gmail.com>
2026-09-17 16:51:34 -0500

    Implementar la validación de datos en la función validar_datos.

--- validaciones.py
+++ validaciones.py
@@ -2,5 +2,13 @@ def validar_datos(nombre, dni, asunto):
     """Retorna True si los datos son validos, False si no.
     Reglas: nombre no vacio, dni tiene exactamente 8 caracteres, asunto no vacio.
     """
+    def validar_datos(nombre, dni, asunto):
+    if not nombre:
+        return False
+    if len(dni) != 8:
+        return False
+    if not asunto:
+        return False
+    return True
     # TODO (Cristobal)
     pass
