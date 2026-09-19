e691e29d9596a6d3918e5c22f9dac5697f100726
Alexander <keomacalderon60@gmail.com>
2026-09-19 18:02:09 -0500

    Agregada la funcion de buscar expedientes

--- procesamiento.py
+++ procesamiento.py
@@ -3,7 +3,7 @@ from persistencia import guardar_en_archivo
 def buscar_posicion(expedientes, codigo):
     """Retorna el indice del expediente con ese codigo, o None si no existe."""
     for i in range(len(expedientes)):
-        if expedientes[i].codigo == codigo: 
+        if expedientes[i]["codigo"] == codigo:
             return i
     return None
     # TODO (Alex) - subir primero, Cristobal depende de esta funcion
@@ -11,11 +11,18 @@ def buscar_posicion(expedientes, codigo):
 
 def buscar_expediente(expedientes):
     """Pide un codigo, usa buscar_posicion() y muestra el resultado."""
+    codigo = int(input("Ingrese codigo a buscar: "))
+    posicion = buscar_posicion(expedientes, codigo)
+    if posicion != None:
+        print(f'Expediente N°: {posicion}')
+    else:
+        print("Expediente no encontrado")
     # TODO (Alex)
     pass
 
 def ordenar_expedientes(expedientes, criterio="codigo"):
     """Ordena con algoritmo BURBUJA (no usar sorted() ni list.sort())."""
+
     # TODO (Alex)
     pass
 
